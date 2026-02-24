b6ac5ff77d3ae5a7be99869b9b211cc7e62f197f
Bhavika Chhatbar <bhavika.chhatbar@tssconsultancy.com>
2026-02-24 18:24:24 +0530

    chagnes

--- src/main/java/model/MenuItem.java
+++ src/main/java/model/MenuItem.java
@@ -1,11 +1,49 @@
 package model;
 
-import java.util.List;
+import util.IdGenerator;
 
-public interface MenuItem {
-    Integer getItemId();
-    String getItemName();
-    Double getItemPrice();
-    String getItemDescription();
-    List getItemAddOnList();
+public class MenuItem {
+    private static final IdGenerator idGenerator = new IdGenerator();
+    private final Integer itemId;
+    private String itemName;
+    private Double itemPrice;
+    private String itemDescription;
+
+    public MenuItem(){
+        this.itemId = idGenerator.generateId();
+    }
+
+    public MenuItem(String itemName, Double itemPrice) {
+        this();
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+    }
+
+    
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    
+    public String getItemName() {
+        return itemName;
+    }
+
+    
+    public Double getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(Double itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    
+    public String getItemDescription() {
+        return itemDescription;
+    }
 }
